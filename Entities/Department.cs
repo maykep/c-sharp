d15f6bd1f13f0634f712059aa131786635257918
Mayke Paiva <maykepaiva.mgap@gmail.com>
2020-10-13 16:22:08 -0300

    Exercicio 1 de Enumeração Realizado 13/10/2020

---
+++ Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace Course.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public Department()
+        {
+
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
